5760c916bb8bf5c77e8633420a57116104867bf3
Alia Wilkinson <aliawilkinson@gmail.com>
2018-11-15 10:02:53 -0800

    js labs

--- Week3/Lab-GraphNode/src/com/alia/graph/Graph.java
+++ Week3/Lab-GraphNode/src/com/alia/graph/Graph.java
@@ -1,10 +1,15 @@
 package com.alia.graph;
 
+import java.util.*;
+
 public class Graph {
 	
 	public static void main(String[] args) {
+		GraphNode g = new GraphNode(10);
+		g.addEdge(0, 2, 10);
+		g.addEdge(1, 3, 11);
+		g.addEdge(2, 4, 12);
+		
+		System.out.println(g);
 	}
-//    private List<GraphNode> nodes;
-
-    // getters, setters, utility methods
 }
--- Week3/Lab-GraphNode/src/com/alia/graph/GraphNode.java
+++ Week3/Lab-GraphNode/src/com/alia/graph/GraphNode.java
@@ -1,8 +1,45 @@
 package com.alia.graph;
 
-public class GraphNode {
-    private int data;
-//    private List<GraphNode> neighbors;
+import java.util.*;
 
-    // getters, setters, and utility methods
-}
+public class GraphNode {
+	class Edge{
+		int v, w;
+		public Edge(int v, int w) {
+			this.v = v; 
+			this.w = w;
+		}
+		@Override
+		public String toString() {
+			return "("+v+""+""+w+")";
+		}
+	}
+	List<Edge> G[];
+	public GraphNode(int n) {
+		G = new LinkedList[n];
+		for(int i = 0; i < G.length; i++) {
+			G[i] = new LinkedList<Edge>();
+		}
+	}
+	boolean isConnected(int u, int v) {
+		for(Edge i: G[u]) {
+			if(i.v == v) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return false;
+	}
+	void addEdge(int u, int v, int w) {
+		G[u].add(0, new Edge(v,w));
+	}
+		@Override
+		public String toString() {
+			String result = "";
+			for(int i =0; i < G.length; i++) {
+				result +=i+"=>"+G[i]+"\n";
+			}
+			return result;
+		}
+	}
